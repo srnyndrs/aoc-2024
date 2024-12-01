1718b1be96ec89ee2f5c227ca209fdaf7623b362
Surányi András <srnyndrs@gmail.com>
2024-12-01 06:46:32 +0100

    Day01 solution

--- src/day_01/Day01.kt
+++ src/day_01/Day01.kt
@@ -1,14 +1,40 @@
 package day_01
 
 import utils.Puzzle
+import kotlin.math.abs
 
 object Day01: Puzzle("01") {
+
+    private fun parseInput(input: List<String>): List<Pair<Long, Long>> {
+        return input.map { line ->
+            line.trim()
+                .split("\\s+".toRegex())
+                .let { (a, b) -> a.toLong() to b.toLong() }
+        }
+    }
+
     override fun part1(input: List<String>): String {
-        return input.joinToString(" ")
+        val pairs = parseInput(input)
+
+        val left = pairs.map { it.first }.sorted()
+        val right = pairs.map { it.second }.sorted()
+
+        return left.zip(right)
+            .sumOf { abs(it.second - it.first) }
+            .toString()
     }
 
     override fun part2(input: List<String>): String {
-        return input.map{ it .toInt() * 10 }.joinToString(" ")
+        val pairs = parseInput(input)
+
+        val left = pairs.map { it.first }
+        val right = pairs.map { it.second }
+
+        return left.sumOf { leftValue ->
+            leftValue * right.count { rightValue ->
+                rightValue == leftValue
+            }
+        }.toString()
     }
 }
 
